d7355ada59ce15970842167038e2eb04349adf7b
abimjoshi7 <abimjoshi7@gmail.com>
2020-06-04 11:00:42 +0545

    second commit

--- wt_conv.py
+++ wt_conv.py
@@ -1,5 +1,11 @@
-wt=input('what is your weight(lbs)?: ')
-new_wt=float(wt)/2.20
-res=str(new_wt)
-print(res[0:4] + "kg")
+wt=float(input("Weight: "))
+z=input("kg or lbs: ")
+q="kg"
+w="lbs"
+if z==q:
+  res=str(wt*2.205)
+  print(res[0:5]+" lbs")
+elif z==w:
+  res=str(wt/2.205)
+  print(res[0:5]+" kg")
 
