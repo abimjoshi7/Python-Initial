33491a41fc936c87014a30a716dcd5a58f9283b5
abimjoshi7 <abimjoshi7@gmail.com>
2020-06-05 19:53:30 +0545

    game on

---
+++ mygame.py
@@ -0,0 +1,5 @@
+while True:
+  player=input("Welcome Sire. Please enter your name: ")
+  print(f'Welcome to the Land of Mythics {player}. Be prepared to conquer your way through.')
+  weapon=input("Choose your weapon-'Strength' 'Knowledge' 'Luck': ")
+  
